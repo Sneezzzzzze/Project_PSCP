44b5f7fd483537397289f5b43a7e69c968cd2975
Chirakit <65070034@kmitl.ac.th>
2022-11-15 21:43:30 +0700

    Update Background.py

--- Background.py
+++ Background.py
@@ -2,9 +2,18 @@
 import pygame
 from pygame.locals import *
 pygame.init()
+
 clock = pygame.time.Clock()
 fps = 60
 width = 540
 height = 700
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Moai\'s revenger')
+
+#define game variables
+ground_scroll = 0
+scroll_speed = 4
+
+#load img
+bg = pygame.image.load('img/Sprite-0003.png')#Not work yet until we got all files.
+ground_img = pygame.image.load('img/nothing.png')#this one too.
